d4ee25eafa9d1ca250a53d0bdf9e94ab6fe1ef63
langbnj <langbnj@gmail.com>
2020-09-26 23:32:24 +0100

    initial

--- include/mysql.R
+++
@@ -1,9 +0,0 @@
-library(DBI)
-library(RMySQL)
-superreserveddrv = dbDriver("MySQL")
-superreservedcon = dbConnect(superreserveddrv, host="localhost", port=3306, dbname="blang", user="username", pass="password")
-
-Query <- function(query)
-{
-  a = dbGetQuery(superreservedcon, statement=query)
-}
